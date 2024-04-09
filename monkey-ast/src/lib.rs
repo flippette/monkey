d74342a6db3904d85fc8fcac334235a1cf38456a
flippette <quangdatle2006@outlook.com>
2024-04-09 09:36:23 +0700

    monkey-ast: fix dead link in lib.rs

--- monkey-ast/src/lib.rs
+++ monkey-ast/src/lib.rs
@@ -1,4 +1,4 @@
 //! An AST for the Monkey programming language.
 
-/// AST utilities, notably [`util::SeekLexer`] and [`util::ReverseSeekLexer`].
+/// AST utilities, notably [`util::SeekLexer`] and [`util::Reverse`].
 pub mod util;
